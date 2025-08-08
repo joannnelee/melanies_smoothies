15170906f6cd34ebaa604db53e8aaa7c26ed2021
Joanne Lee <104439880+joannnelee@users.noreply.github.com>
2025-08-08 14:59:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,11 @@ if ingredients_list:
        
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
+#new section to display smoothie nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 
 
 
